f497d341bfd61b454a58f683288027abe8dfb5a7
juan2305 <78822907+juan2305@users.noreply.github.com>
2021-09-24 22:52:42 -0400

    Propertychanged

---
+++ APISampleIntegration/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace APISampleIntegration.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ APISampleIntegration/ViewModels/FlightViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APISampleIntegration.ViewModels
+{
+   public class FlightViewModel : BaseViewModel
+    {
+        public string AirLineCode { get; set; }
+        public FlightViewModel()
+        {
+
+        }
+    }
+}
